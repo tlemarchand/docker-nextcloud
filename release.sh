fb1f4b15bc77a9ad7325c52901d5d6a92db03392
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2023-01-23 09:04:07 +0100

    Version update

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git checkout master
-echo -n '25.0.2' > version
+curl -s https://api.github.com/repos/nextcloud/server/releases/latest | jq -j -r '.name'| cut -c 2- > version
 git add -A
-git commit -m `cat version`
+git commit -m "Version update"
 git push origin master
 git tag `cat version` -f
 git push origin `cat version` -f
