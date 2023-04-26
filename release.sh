ec5c21b5081836d4380ed9290e02b2ba254ddd9f
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2023-04-26 09:42:43 +0200

    Version update

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git checkout master
-curl -s https://api.github.com/repos/nextcloud/server/releases/latest | jq -j -r '.tag_name'| cut -c 2- > version
+#curl -s https://api.github.com/repos/nextcloud/server/releases/latest | jq -j -r '.tag_name'| cut -c 2- > version
 git add -A
 git commit -m "Version update"
 git push origin master
